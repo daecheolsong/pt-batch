5e72b5bcbc536773a36b0cfc210966e2933b6004
daecheolsong <eocjf8905@naver.com>
2024-01-29 23:21:21 +0900

    #7 - 수업 후 이용권 차감
    - `Booking` 의 status 와 이용권 이용 이력에 따라 이와연관된 `pass`(이용권) 의 개수 차감
    - `AsyncItemProcessor`, `AsyncItemWriter`를 이용하여 비동기 처리

--- src/main/java/com/example/ptbatch/repository/booking/Booking.java
+++ src/main/java/com/example/ptbatch/repository/booking/Booking.java
@@ -1,10 +1,9 @@
 package com.example.ptbatch.repository.booking;
 
 import com.example.ptbatch.repository.BaseEntity;
+import com.example.ptbatch.repository.pass.Pass;
 import com.example.ptbatch.repository.user.User;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,34 +12,47 @@ import java.time.LocalDateTime;
  * @author daecheol song
  * @since 1.0
  */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Setter
 @Entity
 @Table(name = "booking")
 public class Booking extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer bookingSeq;
+    @Setter
     private Integer passSeq;
+    @Setter
     private String userId;
 
     @Enumerated(EnumType.STRING)
+    @Setter
     private BookingStatus status;
+    @Setter
     private boolean usedPass;
+    @Setter
     private boolean attended;
 
+    @Setter
     private LocalDateTime startedAt;
+    @Setter
     private LocalDateTime endedAt;
+    @Setter
     private LocalDateTime cancelledAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", insertable = false, updatable = false)
     private User user;
 
-    public static Booking of(Integer passSeq, String userId, BookingStatus status, boolean usedPass, boolean attended, LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime cancelledAt, User user) {
-        return new Booking(passSeq, userId, status, usedPass, attended, startedAt, endedAt, cancelledAt, user);
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Setter
+    @JoinColumn(name = "passSeq", insertable = false, updatable = false)
+    private Pass pass;
+
+    public static Booking of(Integer passSeq, String userId, BookingStatus status, boolean usedPass, boolean attended, LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime cancelledAt, User user, Pass pass) {
+        return new Booking(passSeq, userId, status, usedPass, attended, startedAt, endedAt, cancelledAt, user, pass);
     }
-    private Booking(Integer passSeq, String userId, BookingStatus status, boolean usedPass, boolean attended, LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime cancelledAt, User user) {
+    private Booking(Integer passSeq, String userId, BookingStatus status, boolean usedPass, boolean attended, LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime cancelledAt, User user, Pass pass) {
         this.passSeq = passSeq;
         this.userId = userId;
         this.status = status;
@@ -50,5 +62,6 @@ public class Booking extends BaseEntity {
         this.endedAt = endedAt;
         this.cancelledAt = cancelledAt;
         this.user = user;
+        this.pass = pass;
     }
 }
